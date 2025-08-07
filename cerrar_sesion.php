84a8956ecf21bfe90aeac7aec644e36f0d7957d2
AleMoorgan <aangelresendizhdz@gmail.com>
2025-08-07 15:03:47 -0600

    Delete cerrar_sesion.php

--- cerrar_sesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Inicia la sesión si aún no está iniciada
-session_unset(); // Elimina todas las variables de sesión
-session_destroy(); // Destruye la sesión
-
-// Redirige al login u otra página principal
-header("Location: index.php");
-exit;
-?>
